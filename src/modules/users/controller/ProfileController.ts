69dc6a6735f19ba6b0d210ed2073cf0340834f7e
Guilherme Freire <guilherme.freire@totvs.com.br>
2022-04-29 08:12:25 -0300

    Show and Update Profile Users

---
+++ src/modules/users/controller/ProfileController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+export default class ProfileController {
+    public async show(request: Request, response: Response): Promise<Response> {
+        const showProfileService = new ShowProfileService();
+
+        const user_id = request.user.id;
+
+        const profileUser = await showProfileService.execute({ user_id });
+
+        return response.json(profileUser);
+    }
+
+    public async update(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const updateProfileService = new UpdateProfileService();
+        const user_id = request.user.id;
+        const { name, email, password, old_password } = request.body;
+
+        const updateProfile = await updateProfileService.execute({
+            user_id,
+            name,
+            email,
+            password,
+            old_password,
+        });
+
+        return response.json(updateProfile);
+    }
+}
