04ccae8226533c69d937da2f55e666a7b2db8c47
Guilherme Freire <guilherme.freire@totvs.com.br>
2022-05-04 23:08:45 -0300

    Update Customers and Create modules Orders

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRoutes from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customerRoutes from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customerRoutes);
 
 export default routes;
