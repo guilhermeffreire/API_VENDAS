deb4dc3d8ad2b950a824f15d952662a407a5b727
Guilherme Freire <guilherme.freire@totvs.com.br>
2022-05-24 22:40:08 -0300

    Add Cache with Redis

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -23,11 +24,15 @@ class CreateProductService {
             throw new AppError('There is already one product with this name');
         }
 
+        const redisCache = new RedisCache();
+
         const newProduct = productsRepository.create({
             name,
             price,
             quantity,
         });
+
+        await redisCache.invalidate('api-vendas-PRODUCTS_LIST');
 
         await productsRepository.save(newProduct);
 
--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -1,12 +1,23 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductsService {
     public async execute(): Promise<Product[]> {
         const productsRepository = getCustomRepository(ProductRepository);
 
-        const products = await productsRepository.find();
+        const redisCache = new RedisCache();
+
+        let products = await redisCache.recover<Product[]>(
+            'api-vendas-PRODUCTS_LIST',
+        );
+
+        if (!products) {
+            products = await productsRepository.find();
+
+            await redisCache.save('api-vendas-PRODUCTS_LIST', products);
+        }
 
         return products;
     }
